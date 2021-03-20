4f9d43f7d93ddbbf83f25a929974f5bd9c6523e2
Reptetion <notaxku@gmail.com>
2021-03-19 19:53:57 -0600

    added a check to see if it exist

--- manager.js
+++ manager.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-async function Premium() {
+async function activatePremium() {
 const { data: responses } = await axios.get("https://dev.sellix.io/v1/orders", {
 headers: {
 Authorization: "Bearer 6OHm5ZVayYmAbKnxXFVZmbUZS0egDBPwj3r8qzbxxJCMKn4PfTiyyGp6pXHf2sOo"
@@ -8,5 +8,7 @@ Authorization: "Bearer 6OHm5ZVayYmAbKnxXFVZmbUZS0egDBPwj3r8qzbxxJCMKn4PfTiyyGp6p
 })
 const { custom_fields } = responses.data.orders[0]
 if(responses.data.status == 'PENDING') return;
-await bot.database.set(`${custom_fields['Discord ID']}.premiumuser`, true)
+if(!await bot.database.exists(`${custom_fields['Discord ID']}.premiumuser`)) await bot.database.set(`${custom_fields['Discord ID']}.premiumuser`, true)
+
+//add a check to see if the user is in the support server and give them the donator role 
 }
