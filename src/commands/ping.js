d1714847475d1ac0bb2d4dddbb7715e653777680
Reptetion <notaxku@gmail.com>
2021-04-05 12:21:50 -0600

    just a ping cmd

---
+++ src/commands/ping.js
@@ -0,0 +1,20 @@
+exports.run = async(bot, message, args) => {
+    
+let ping = await bot.database.fetchLatency()
+let ping2 = await bot.subdb.fetchLatency()
+    
+message.channel.send('Getting Ping...')
+.then(msg => {
+setTimeout(function() {
+msg.edit(`Bot: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms\nAPI: ${Math.round(bot.ws.ping)}ms\nMainDB: ${Math.round(ping.average)}ms\nSubDB: ${Math.round(ping2.average)}ms`)
+}, 100)
+})
+    
+}
+
+exports.config = {
+name: 'ping',
+description: 'Pong!',
+usage: 'ping',
+aliases: []
+}
