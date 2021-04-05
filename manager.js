a4fc5001642810c75dbb17894469480ce73e2570
Reptetion <notaxku@gmail.com>
2021-04-05 10:43:59 -0600

    package.json and manager update

--- manager.js
+++ manager.js
@@ -1,6 +1,10 @@
-const { Client, Collection } = require("discord.js");
+const { Client, Collection, MessageEmbed } = require("discord.js");
 
 const { Database } = require("quickmongo");
+
+const DBL = require("dblapi.js");
+
+const axios = require('axios');
 
 const chalk = require("chalk");
 
@@ -28,88 +32,20 @@ intents: [
 
 bot.commands = new Collection()
 bot.aliases = new Collection()
-bot.events = new Collection()
 
 bot.config = require('./config.json')
 
 bot.database = new Database(bot.config.dbURL)
 
-fs.readdir("./src/developer/", (err, files) => {
-files.forEach(file => {
-if(!file.endsWith(".js")) return;
-let command = require(`./src/developer/${file}`)
-try {
-if(command.config) {
-bot.commands.set(command.config.name, command)
-command.config.aliases.forEach(alias => bot.aliases.set(alias, command.config.name))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${command.config.name} has been loaded.`))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${bot.commands.size} are ready.`))
-}
-} catch (e) {
-console.log(chalk.bgRed('[Bot]:') + chalk.red(` ${command.config.name} could not load properly. Error: ${e}`))
-}
-})
-})
+bot.dbl = new DBL(bot.config.dbltoken, { webhookPort: 4083, webhookAuth: '8550' })
 
-fs.readdir("./src/premium/", (err, files) => {
+fs.readdir("./src/commands/", (err, files) => {
 files.forEach(file => {
 if(!file.endsWith(".js")) return;
-let command = require(`./src/premium/${file}`)
+let command = require(`./src/commands/${file}`)
 try {
 if(command.config) {
 bot.commands.set(command.config.name, command)
-command.config.aliases.forEach(alias => bot.aliases.set(alias, command.config.name))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${command.config.name} has been loaded.`))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${bot.commands.size} are ready.`))
-}
-} catch (e) {
-console.log(chalk.bgRed('[Bot]:') + chalk.red(` ${command.config.name} could not load properly. Error: ${e}`))
-}
-})
-})
-
-fs.readdir("./src/settings/", (err, files) => {
-files.forEach(file => {
-if(!file.endsWith(".js")) return;
-let command = require(`./src/settings/${file}`)
-try {
-if(command.config) {
-bot.commands.set(command.config.name, command)
-command.config.aliases.forEach(alias => bot.aliases.set(alias, command.config.name))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${command.config.name} has been loaded.`))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${bot.commands.size} are ready.`))
-}
-} catch (e) {
-console.log(chalk.bgRed('[Bot]:') + chalk.red(` ${command.config.name} could not load properly. Error: ${e}`))
-}
-})
-})
-
-fs.readdir("./src/staff/", (err, files) => {
-files.forEach(file => {
-if(!file.endsWith(".js")) return;
-let command = require(`./src/staff/${file}`)
-try {
-if(command.config) {
-bot.commands.set(command.config.name, command)
-command.config.aliases.forEach(alias => bot.aliases.set(alias, command.config.name))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${command.config.name} has been loaded.`))
-console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${bot.commands.size} are ready.`))
-}
-} catch (e) {
-console.log(chalk.bgRed('[Bot]:') + chalk.red(` ${command.config.name} could not load properly. Error: ${e}`))
-}
-})
-})
-
-fs.readdir("./src/utility/", (err, files) => {
-files.forEach(file => {
-if(!file.endsWith(".js")) return;
-let command = require(`./src/utility/${file}`)
-try {
-if(command.config) {
-bot.commands.set(command.config.name, command)
-bot.settings.set(command.config.name, command)
 command.config.aliases.forEach(alias => bot.aliases.set(alias, command.config.name))
 console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${command.config.name} has been loaded.`))
 console.log(chalk.bgGreen('[Bot]:') + chalk.green(` ${bot.commands.size} are ready.`))
@@ -138,35 +74,27 @@ console.log(chalk.bgRed('[Bot]:') + chalk.red(` ${event} could not load properly
 process.on('warning', console.warn)
 process.on('unhandledRejection', error => {
 console.log(chalk.bgRed('[Error]:') + chalk.red(` ${error}`))
+}) 
+
+bot.dbl.webhook.on('ready', hook => {
+console.log(`Webhook running!`);
 })
 
-const DBL = require("dblapi.js")   
-
-const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDU2MTE4ODgyMjM4NDY1NiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA0MjU5NDU2fQ.-5-78ddMMYTSMaYdWOfRlSxWoJzT-_tvP17vuD-FV4U', { webhookPort: 8369, webhookAuth: "8550" })
-
-dbl.webhook.on('ready', hook => {
-console.log(`Webhook running!`);
-});
-
 dbl.webhook.on('vote', vote => {
-let tyforvoting = new Discord.MessageEmbed()
-.setColor("#7777f4")
-.setTitle("Someone has voted on top.gg!")
+let tyforvoting = new MessageEmbed()
+.setColor(bot.config.color)
+.setTitle('Someone has voted on top.gg!')
 .setURL('https://top.gg/bot/634561188822384656/vote')
 .setDescription(`<:wumpus_luv:724672126002528298> Thank you for voting, <@${vote.user}>! You have received some rewards, Enjoy!\n\n• Access to \`re!vote claim\`\n• **Supporter** badge on your profile\n• 25% XP Boost\n\nRemember to vote every 12 hours so you can keep all those goodies!`)
 .setFooter("Thank You For Supporting ReCreate!")
-bot.channels.cache.get("767103210539188286").send(tyforvoting);
+bot.channels.cache.get("767103210539188286").send(tyforvoting)
 let guild = bot.guilds.cache.get('658824686997733399');
-let role = guild.roles.cache.find(r => r.name == "ReCreate Voter");
-let member = guild.members.cache.find(v => v.id == vote.user);
+let role = guild.roles.cache.find(r => r.name == 'Voted')
+let member = guild.members.cache.find(v => v.id == vote.user)
 if(member) {
 member.roles.add(role).catch(console.error)
 }
-});
-
-const axios = require('axios');
-
-const chalk = require('chalk');
+})
 
 async function activatePremium() {
 
@@ -179,7 +107,12 @@ const { custom_fields } = responses.data.orders[0]
 if(responses.data.status == 'PENDING') return;
 if(!await bot.subdb.exists(`${custom_fields['Discord ID']}.premiumuser`)) await bot.subdb.set(`${custom_fields['Discord ID']}.premiumuser`, true)
 
-//add a check to see if the user is in the support server and give them the donator role 
+let guild = bot.guilds.cache.get('658824686997733399');
+let role = guild.roles.cache.find(r => r.name == 'Donator')
+let member = guild.members.cache.find(v => v.id == vote.user)
+if(member) {
+member.roles.add(role).catch(console.error)
+}
 
 console.log(chalk.bgGreen('[User]:') + chalk.green(` Premium has been activated for ${ID}.`))
 
