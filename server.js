cf58768548599d12384a837b6f45d9a8534fe695
Reptetion <notaxku@gmail.com>
2021-04-24 19:13:40 -0600

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get("/", (request, response) => {
 console.log(Date.now() + " ping recieved.");
 response.sendStatus(200);
 });
-app.listen(22)
+app.listen(4268)
 setInterval(() => {
-http.get(`http://66.11.118.236:22/`);
+http.get(`http://66.11.118.236:4268/`);
 }, 280000);
