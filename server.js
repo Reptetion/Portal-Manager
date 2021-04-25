11bc77fd666282a2dc1c63b9fe9e859cf0954e55
Reptetion <notaxku@gmail.com>
2021-04-24 19:25:26 -0600

    portal manager maintenance done

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const http = require("http")
 const express = require('express');
+
+const http = require('http');
+
 const app = express();
+
+const chalk = require('chalk');
+
 app.get("/", (request, response) => {
-console.log(Date.now() + " ping recieved.");
-response.sendStatus(200);
-});
+console.log(chalk.bgGreen('[UptimeRobot]:') + chalk.green(' Ping received.'))
+response.sendStatus(200)
+})
 app.listen(4268)
 setInterval(() => {
-http.get(`http://66.11.118.236:4268/`);
-}, 280000);
+http.get(`http://66.11.118.236:4268/`)
+}, 300000)
